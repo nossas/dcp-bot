476bf150356d42fd1ddb1698369ed50128d8b496
willow <brmagrini@gmail.com>
2025-04-03 01:36:10 -0300

    Fluxo de compartilhar risco completo

--- actions/actions.py
+++ actions/actions.py
@@ -21,17 +21,24 @@ class ActionFallbackButtons(Action):
     def run(self, dispatcher, tracker, domain):
         logger.debug("Fallback! fallback!!")
 
+        last_action = None
+        for event in reversed(tracker.events):
+            if event.get("event") == "action" and event.get("name") not in ["action_listen","action_repeat_last_message","action_fallback_buttons"]:
+                last_action = event.get("name")
+                break
+        logger.debug(f"Last action:{last_action}")
+        if last_action == "action_perguntar_nome":
+            user_message = tracker.latest_message.get("text")
+            logger.debug(f"Salvando fallback como nome: {user_message}")
+            return [
+                FollowupAction("action_salvar_nome")
+            ]
+
         nome = tracker.get_slot("nome")
         if not nome:
             return [FollowupAction("action_perguntar_nome")]
 
         # Verifica se a última ação foi action_ask_descricao_risco
-        last_action = None
-        for event in reversed(tracker.events):
-            if event.get("event") == "action":
-                last_action = event.get("name")
-                break
-
         if last_action == "action_ask_descricao_risco":
             user_message = tracker.latest_message.get("text")
             logger.debug(f"Salvando fallback como descrição de risco: {user_message}")
@@ -116,24 +123,158 @@ class ActionRepeatLastMessage(Action):
 
     def run(self, dispatcher, tracker, domain):
         last_bot_messages = []
-
+        last_action = None
         for event in reversed(tracker.events):
-            if event.get("event") == "bot":
-                last_bot_messages.append(event.get("text"))
-                if len(last_bot_messages) == 2:
-                    break
+            if event.get("event") == "action" and event.get("name") not in ["action_listen","action_repeat_last_message","action_fallback_buttons"]:
+                last_action = event.get("name")
+                break
+        logger.debug(f"Last action:{last_action}")
+        
+        # for event in reversed(tracker.events):
+        #     if event.get("event") == "bot":
+        #         last_bot_messages.append(event.get("text"))
+        #         if len(last_bot_messages) == 2:
+        #             break
 
-        if len(last_bot_messages) == 2:
-            penultima_mensagem = last_bot_messages[1]
-            dispatcher.utter_message(text=penultima_mensagem)
-        elif last_bot_messages:
-            dispatcher.utter_message(text=last_bot_messages[0])
+        # if len(last_bot_messages) == 2:
+        #     penultima_mensagem = last_bot_messages[1]
+        #     dispatcher.utter_message(text=penultima_mensagem)
+        if last_action:
+            dispatcher.utter_message("Vamos tentar de novo.")
+            return [FollowupAction(last_action)]
         else:
             dispatcher.utter_message(text="Desculpe, não consigo repetir a última mensagem.")
+            return [FollowupAction("utter_menu_inicial")]
 
+class ActionRetomaRisco(Action):
+    def name(self) -> str:
+        return "action_retoma_risco"
+
+    def run(self, dispatcher,
+            tracker,
+            domain):
+        slots = {
+            "classificacao_risco": tracker.get_slot("classificacao_risco"),
+            "descricao_risco": tracker.get_slot("descricao_risco"),
+            "endereco": tracker.get_slot("endereco"),
+            "midias": tracker.get_slot("midias"),
+            "identificar": tracker.get_slot("identificar"),
+        }
+        if slots['endereco']:
+            if slots['classificacao_risco']:
+                return[FollowupAction("action_ask_descricao_risco")]
+            return[FollowupAction("utter_classificar_risco")]
+        return[FollowupAction("action_request_location")]
+    
+class ActionPerguntarNome(Action):
+    def name(self):
+        return "action_perguntar_nome"
+
+    def run(self, dispatcher, tracker, domain):
+        sender_id = tracker.sender_id
+        nome = tracker.get_slot("nome")
+        if nome:
+            logger.error(f"Nome no Slot")
+
+            dispatcher.utter_message(
+                text=f"O nome {nome} está correto?",
+                buttons=[
+                    {"title": "Sim", "payload": "/affirm_name"},
+                    {"title": "Não", "payload": "/deny_name"}
+                ]
+            )
+            return []
+
+        try:
+            
+            conn = get_db_connection()
+            if conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT nome FROM usuarios WHERE whatsapp_id = %s", (sender_id,))
+                row = cursor.fetchone()
+                cursor.close()
+                conn.close()
+
+                if row:
+                    logger.error(f"Nome no banco")
+
+                    nome = row[0]
+                    dispatcher.utter_message(
+                        text=f"O nome {nome} está correto?",
+                        buttons=[
+                            {"title": "Sim", "payload": "/affirm_name"},
+                            {"title": "Não", "payload": "/deny_name"}
+                        ]
+                    )
+                    return [SlotSet("nome", nome)]
+
+        except Exception as e:
+            logger.error(f"Erro ao buscar nome no banco: {e}")
+
+        dispatcher.utter_message(
+            text="Olá! Aqui é o chatbot da Defesa Climática Popular. Como posso te chamar?\nNão se preocupe pois manteremos o sigilo."
+        )
         return []
 
+class ActionSalvarNome(Action):
+    def name(self):
+        return "action_salvar_nome"
 
+    def run(self, dispatcher, tracker, domain):
+        nome = tracker.latest_message.get("text") if tracker.latest_message.get("text") != '/affirm_name' else tracker.get_slot("nome")
+        whatsapp_id = tracker.sender_id
+
+        if nome:
+            try:
+                conn = get_db_connection()
+                if conn:
+                    cursor = conn.cursor()
+                    cursor.execute("""
+                        INSERT INTO usuarios (nome,whatsapp_id)
+                        VALUES (%s, %s)
+                        ON CONFLICT (whatsapp_id) DO UPDATE SET nome = EXCLUDED.nome
+                    """, (nome,whatsapp_id))
+                    conn.commit()
+                    cursor.close()
+                    conn.close()
+                    return[FollowupAction("action_perguntar_nome")]
+                else:
+                    dispatcher.utter_message(text="Erro ao conectar ao banco de dados.")
+            except Exception as e:
+                dispatcher.utter_message(text="Ocorreu um erro ao salvar seu nome.")
+                print(f"[ERRO BANCO] {e}")
+        else:
+            dispatcher.utter_message(text="Não consegui entender seu nome.")
+
+        return [SlotSet("nome", nome)]
+
+class ActionApagarNome(Action):
+    def name(self):
+        return "action_apagar_nome"
+
+    def run(self, dispatcher, tracker, domain):
+        logger.debug("Apagando nome do slot e do banco.")
+
+        whatsapp_id = tracker.sender_id
+
+        try:
+            conn = get_db_connection()
+            if conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    "DELETE FROM usuarios WHERE whatsapp_id = %s;",
+                    (whatsapp_id,)
+                )
+                conn.commit()
+                logger.debug(f"Nome apagado do banco para {whatsapp_id}")
+        except Exception as e:
+            logger.error(f"Erro ao apagar nome do banco: {e}")
+            dispatcher.utter_message(text="Ocorreu um erro ao apagar seu nome do sistema.")
+        finally:
+            if conn:
+                conn.close()
+        return [SlotSet("nome", None)]
+    
 class ActionBuscarEnderecoOpenStreet(Action):
     def name(self):
         return "action_buscar_endereco_openstreet"
@@ -162,22 +303,22 @@ class ActionBuscarEnderecoOpenStreet(Action):
                     dispatcher.utter_message(
                         text=f"Encontrei esse endereço: {endereco}\n Está correto?",
                         buttons=[
-                            {"title": "Sim", "payload": "/affirm"},
-                            {"title": "Não", "payload": "/deny"}
+                            {"title": "Sim", "payload": "/affirm_address"},
+                            {"title": "Não", "payload": "/deny_address"}
                         ]
                     )
                     return [SlotSet("latitude", latitude), SlotSet("longitude", longitude), SlotSet("endereco", endereco)]
                 else:
                     dispatcher.utter_message(text="Desculpe, não consegui encontrar o endereço.tente novamente.")
-                    return [FollowupAction("action_perguntar_nome")]
+                    return [FollowupAction("action_request_location")]
             else:
                 logger.debug(f"tem endereço")
                 endereco = location_data['address']
                 dispatcher.utter_message(
                     text=f"Encontrei esse endereço: {endereco}\n Está correto?",
                     buttons=[
-                        {"title": "Sim", "payload": "/affirm"},
-                        {"title": "Não", "payload": "/deny"}
+                        {"title": "Sim", "payload": "/affirm_address"},
+                        {"title": "Não", "payload": "/deny_address"}
                     ]
                 )
                 return [SlotSet("latitude", latitude), SlotSet("longitude", longitude), SlotSet("endereco", endereco)]
@@ -195,6 +336,7 @@ class ActionBuscarEnderecoOpenStreet(Action):
                 ]
             )
             return []
+
 class ActionBuscarEnderecoTextoOpenStreet(Action):
     def name(self):
         return "action_buscar_endereco_texto_openstreet"
@@ -219,133 +361,17 @@ class ActionBuscarEnderecoTextoOpenStreet(Action):
                 dispatcher.utter_message(
                     text=f"Encontrei esse endereço: {endereco}\n Está correto?",
                     buttons=[
-                        {"title": "Sim", "payload": "/affirm"},
-                        {"title": "Não", "payload": "/deny"}
+                        {"title": "Sim", "payload": "/affirm_address"},
+                        {"title": "Não", "payload": "/deny_address"}
                     ]
                 )
                 return [SlotSet("latitude", latitude), SlotSet("longitude", longitude), SlotSet("endereco", endereco)]
             else:
-                dispatcher.utter_message(text="Não encontrei esse endereço. Você pode tentar novamente?")
-                return [FollowupAction("action_perguntar_nome")]
+                dispatcher.utter_message(text="Não encontrei esse endereço. tente novamente.")
+                return [FollowupAction("action_request_location")]
         else:
             dispatcher.utter_message(text="Desculpe, não consegui buscar o endereço agora. Vamos tentar de novo.")
-            return [FollowupAction("action_perguntar_nome")]
-
-
-
-class ActionSalvarNome(Action):
-    def name(self):
-        return "action_salvar_nome"
-
-    def run(self, dispatcher, tracker, domain):
-        nome = tracker.get_slot("nome")
-        whatsapp_id = tracker.sender_id
-
-        if nome:
-            try:
-                conn = get_db_connection()
-                if conn:
-                    cursor = conn.cursor()
-                    cursor.execute("""
-                        INSERT INTO usuarios (nome,whatsapp_id)
-                        VALUES (%s, %s)
-                        ON CONFLICT (whatsapp_id) DO UPDATE SET nome = EXCLUDED.nome
-                    """, (nome,whatsapp_id))
-                    conn.commit()
-                    cursor.close()
-                    conn.close()
-                    dispatcher.utter_message(text=f"Prazer em te conhecer, {nome}!")
-                else:
-                    dispatcher.utter_message(text="Erro ao conectar ao banco de dados.")
-            except Exception as e:
-                dispatcher.utter_message(text="Ocorreu um erro ao salvar seu nome.")
-                print(f"[ERRO BANCO] {e}")
-        else:
-            dispatcher.utter_message(text="Não consegui entender seu nome.")
-
-        return [SlotSet("nome", nome)]
-
-class ActionPerguntarNome(Action):
-    def name(self):
-        return "action_perguntar_nome"
-
-    def run(self, dispatcher, tracker, domain):
-        sender_id = tracker.sender_id
-        nome = tracker.get_slot("nome")
-        if nome:
-            logger.error(f"Nome no Slot")
-
-            dispatcher.utter_message(
-                text=f"O nome {nome} está correto?",
-                buttons=[
-                    {"title": "Sim", "payload": "/affirm"},
-                    {"title": "Não", "payload": "/deny"}
-                ]
-            )
-            return []
-
-        try:
-            
-            conn = get_db_connection()
-            if conn:
-                cursor = conn.cursor()
-                cursor.execute("SELECT nome FROM usuarios WHERE whatsapp_id = %s", (sender_id,))
-                row = cursor.fetchone()
-                cursor.close()
-                conn.close()
-
-                if row:
-                    logger.error(f"Nome no banco")
-
-                    nome = row[0]
-                    dispatcher.utter_message(
-                        text=f"O nome {nome} está correto?",
-                        buttons=[
-                            {"title": "Sim", "payload": "/affirm"},
-                            {"title": "Não", "payload": "/deny"}
-                        ]
-                    )
-                    return [SlotSet("nome", nome)]
-
-        except Exception as e:
-            logger.error(f"Erro ao buscar nome no banco: {e}")
-
-        dispatcher.utter_message(
-            text="Olá! Aqui é o chatbot da Defesa Climática Popular. Como posso te chamar?\nNão se preocupe pois manteremos o sigilo."
-        )
-        return []
-
-
-class ActionApagarNome(Action):
-    def name(self):
-        return "action_apagar_nome"
-
-    def run(self, dispatcher, tracker, domain):
-        logger.debug("Apagando nome do slot e do banco.")
-
-        whatsapp_id = tracker.sender_id
-
-        try:
-            conn = get_db_connection()
-            if conn:
-                cursor = conn.cursor()
-                cursor.execute(
-                    "DELETE FROM usuarios WHERE whatsapp_id = %s;",
-                    (whatsapp_id,)
-                )
-                conn.commit()
-                logger.debug(f"Nome apagado do banco para {whatsapp_id}")
-        except Exception as e:
-            logger.error(f"Erro ao apagar nome do banco: {e}")
-            dispatcher.utter_message(text="Ocorreu um erro ao apagar seu nome do sistema.")
-        finally:
-            if conn:
-                conn.close()
-
-        dispatcher.utter_message(text="Tudo bem, como posso te chamar então?")
-        return [SlotSet("nome", None)]
-    
-
+            return [FollowupAction("action_request_location")]
 
 class ActionSalvarClassificacaoRisco(Action):
     def name(self) -> str:
@@ -366,8 +392,6 @@ class ActionSalvarClassificacaoRisco(Action):
         else:
             dispatcher.utter_message(text="Não consegui entender a classificação do risco.")
             return [FollowupAction("utter_classificar_risco")]
-
-
 
 class ActionSolicitarDescricaoRisco(Action):
     def name(self) -> str:
@@ -391,7 +415,6 @@ class ActionSolicitarDescricaoRisco(Action):
             dispatcher.utter_message(text="Não consegui entender a classificação do risco.")
             return [FollowupAction("utter_classificar_risco")]
 
-
 class ActionSalvarDescricaoRisco(Action):
     def name(self) -> str:
         return "action_salvar_descricao_risco"
@@ -405,6 +428,7 @@ class ActionSalvarDescricaoRisco(Action):
         else:
             dispatcher.utter_message(text="Não consegui entender a descrição, tente novamente.")
             return [FollowupAction("action_ask_descricao_risco")]
+
 class ActionSalvarMidiaRisco(Action):
     def name(self) -> str:
         return "action_salvar_midia_risco"
@@ -432,7 +456,6 @@ class ActionSalvarMidiaRisco(Action):
             dispatcher.utter_message(text="Ocorreu um erro ao salvar mídia.")
             logger.error(f"Erro ao salvar mídia no slot: {e}")
             return []
-
 
 class ActionConfirmarRisco(Action):
     def name(self) -> str:
@@ -486,6 +509,16 @@ class ActionSalvarRisco(Action):
                 {"title": "Não", "payload": "/nao_compartilhar_mensagem_risco"}
             ]
         )
+
+        return []
+
+class ActionFinalizaRisco(Action):
+    def name(self) -> str:
+        return "action_finaliza_risco"
+
+    def run(self, dispatcher,
+            tracker,
+            domain):
 
         return [SlotSet("classificacao_risco", None),
         SlotSet("descricao_risco", None),
