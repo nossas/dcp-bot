efbba2dcff14816302d3539ba1df451feacfc194
willow <brmagrini@gmail.com>
2025-04-09 23:43:48 -0300

    adiciona migrations no entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,10 +5,36 @@ python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_DB", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="$POSTGRES_HOST")
+    conn = psycopg2.connect(
+        dbname="$POSTGRES_DB",
+        user="$POSTGRES_USER",
+        password="$POSTGRES_PASSWORD",
+        host="$POSTGRES_HOST"
+    )
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
+END
+}
+
+function should_run_migrations(){
+python << END
+import sys
+import psycopg2
+conn = psycopg2.connect(
+    dbname="$POSTGRES_DB",
+    user="$POSTGRES_USER",
+    password="$POSTGRES_PASSWORD",
+    host="$POSTGRES_HOST"
+)
+cur = conn.cursor()
+cur.execute("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'users');")
+exists = cur.fetchone()[0]
+cur.close()
+conn.close()
+if not exists:
+    sys.exit(0)  # table does not exist: run migrations
+sys.exit(1)      # table exists: skip migrations
 END
 }
 
@@ -17,9 +43,14 @@ until postgres_ready; do
   sleep 1
 done
 
->&2 echo "Banco disponível. Executando migrations.py..."
-
 cd /app
-python migrations.py
-rasa train 
+
+if should_run_migrations; then
+  >&2 echo "Rodando migrations.py porque a tabela 'users' não existe."
+  python migrations.py
+else
+  >&2 echo "Tabela 'users' já existe. Pulando migrations."
+fi
+
+rasa train
 rasa run --connector whatsapp_connector.WhatsAppInput --debug
